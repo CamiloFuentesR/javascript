73e8b9376eb0948fc234481cfbb7adc5cee0d7c8
Tutin R <lokomilo1@gmail.com>
2020-10-24 22:54:14 -0300

    clima-fin

--- clima/js/app.js
+++ clima/js/app.js
@@ -14,24 +14,24 @@ const option =document.createElement('option');
 ciudadSelect.appendChild(option)
 */
 
-buscarPais.addEventListener('change',ciudad); 
+buscarPais.addEventListener('change', ciudad);
 
 
-function obtenerPais(e){
+function obtenerPais(e) {
 
-    
+
     console.log(e.target.value)
 }
 
 // window.addEventListener('DOMContentLoaded',ciudad);
 
 
-window.addEventListener('load',()=>{
-    formulario.addEventListener('submit',buscarClima);
+window.addEventListener('load', () => {
+    formulario.addEventListener('submit', buscarClima);
 });
 
 
-function buscarClima(e){
+function buscarClima(e) {
     e.preventDefault();
 
     //validar
@@ -39,32 +39,32 @@ function buscarClima(e){
     const ciudad = document.querySelector('#ciudad').value;
     const pais = document.querySelector('#pais').value;
 
-    if(ciudad ==='' || pais ===''){
-         mostrarError('Ambos campos son obligtorios');
+    if (ciudad === '' || pais === '') {
+        mostrarError('Ambos campos son obligtorios');
 
-      /*   Swal.fire(
-            'Campos vacìos',
-            '¿Revisaste que todos los campos esten llenos?',
-            'warning'
-          ) */
+        /*   Swal.fire(
+              'Campos vacìos',
+              '¿Revisaste que todos los campos esten llenos?',
+              'warning'
+            ) */
 
         return;
     }
 
     //consultar la API
-    
 
-    consultarApi(ciudad,pais);
+
+    consultarApi(ciudad, pais);
 }
 
 
-function mostrarError(mensaje){
+function mostrarError(mensaje) {
     const alerta = document.querySelector('.bg-red-100');
 
-    if(!alerta){
+    if (!alerta) {
         //el scope de alerta no llega dentro de esta funcion
         const alerta = document.createElement('div');
-        alerta.classList.add('bg-red-100','border-red-400','text-red-700','px4','py3','rounded','max-w-md','mx-auto','mt-6','text-center');
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px4', 'py3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
 
         alerta.innerHTML = `
         <strong class="font-bold">Error!!</strong>
@@ -75,117 +75,147 @@ function mostrarError(mensaje){
 
         setTimeout(() => {
             alerta.remove();
-            
+
         }, 2000);
 
     }
 }
 
-function ciudad(e){
+function ciudad(e) {
     const ciudadValue = e.target.value;
-    
     const url = 'ciudad/city.json';
+    spinner();
 
     fetch(url)
-    .then(respuesta => respuesta.json())
-    .then(datos => mostrarCiudad(datos,ciudadValue))
+        .then(respuesta => respuesta.json())
+        .then(datos =>{
+            limpirHTML();
+            mostrarCiudad(datos, ciudadValue)} )
+        
 }
 
-function mostrarCiudad(datos,id){
+function mostrarCiudad(datos, id) {
     console.log(id)
-    const {country} =datos;
+    const { country } = datos;
     const contenido = document.querySelector('#ciudad');
-    let html =``;
+    let html = ``;
 
     let ciudades = datos.filter(ciudad => id === ciudad.country)
-  
-    
+
+
     ciudades.forEach(nombre => {
-        html +=`
+        html += `
         <option value="${nombre.name}">${nombre.name}</option>`;
-           
-       });
-       contenido.innerHTML =html;
-       $('.select2').select2();
 
- /*    ciudades.forEach(ciudad => {
-    let newCiudades = new Option(ciudad.name,ciudad.id,false,false);
-    $('.select2').append(newCiudades).trigger('change').select2();
-    }); */
-
-
-/*         console.log(ciudades);
- */     /*    for(let nombre of ciudades) {
-           console.log(nombre.name) */
-          
-       /*    ciudades.forEach(ciudad => {
-           const ciudadSelect = document.querySelector('#ciudad');
-
-            const option =document.createElement('option');
-            option.value = ciudad.name;
-            option.textContent = ciudad.name;
-            
-             
-            ciudadSelect.appendChild(option) 
-
-               
-           });  */
-
-          
- /*    */
-
-
-
-/* ciudadSelect.innerHTML = html;
-             */
-        
-
-      
-
-
-     /*    
-
-        if(country ==='CL'){
-            console.log(datos);
-        } */
-
-       
-        // const {id,nombre,empresa,trabajo} = datos;
-
-  /*       html += `
-        --------------------------------------
-        <p>Id : ${id}</p>
-        <p>Nombre : ${nombre}</p>
-        <p>Empresa : ${empresa}</p>
-        <p>Trabajo : ${trabajo}</p>
-        --------------------------------------
-        `;
     });
-     */
-    
-  
- 
+    contenido.innerHTML = html;
+    $('.select2').select2();
+
+    /*    ciudades.forEach(ciudad => {
+       let newCiudades = new Option(ciudad.name,ciudad.id,false,false);
+       $('.select2').append(newCiudades).trigger('change').select2();
+       }); */
+
+    /*    ciudades.forEach(ciudad => {
+        const ciudadSelect = document.querySelector('#ciudad');
+
+         const option =document.createElement('option');
+         option.value = ciudad.name;
+         option.textContent = ciudad.name;
+          
+         ciudadSelect.appendChild(option) 
+        });  */
+
+    // const {id,nombre,empresa,trabajo} = datos;
+
 }
 
 
-        
-    
-
-
-
-
-function consultarApi(ciudad,pais){
+function consultarApi(ciudad, pais) {
     const apiId = 'e2a01a2c23f18128b343c96a3cc180e5';
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${apiId}`;
-
+    spinner();
     fetch(url)
-    .then(respuesta => respuesta.json())
-    .then(datos => {
-        console.log(datos);
-        if(datos.cod ==="404"){
-            mostrarError('Ciudad no encontrada');
-        }
-    })
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+            limpirHTML();//limpiar el htl previo
+            console.log(datos);
+            if (datos.cod === "404") {
+                mostrarError('Ciudad no encontrada');
+                return;
+            }
+
+            mostrarClima(datos);
+        })
 }
 
+function mostrarClima(datos) {
+
+    const { name, main: { temp, temp_max, temp_min } } = datos;
+    const centigrados = kelvinACentigrados(temp);
+    const max = kelvinACentigrados(temp_max);
+    const min = kelvinACentigrados(temp_min);
+
+    const nombreCiudad = document.createElement('p');
+    nombreCiudad.textContent = `Clima en ${name}`;
+    nombreCiudad.classList.add('text-bold', 'text-2xl')
+
+    const actual = document.createElement('p');
+    actual.innerHTML = `${centigrados} &#8451;`;
+    actual.classList.add('font-bold', 'text-6xl');
+
+    const tempMax = document.createElement('p');
+    tempMax.innerHTML = `Max: ${max} &#8451;`;
+    tempMax.classList.add('text-xl');
+
+    const tempMin = document.createElement('p');
+    tempMin.innerHTML = `Min: ${min} &#8451;`;
+    tempMin.classList.add('text-xl');
+
+    const resultadoDiv = document.createElement('div');
+    resultadoDiv.classList.add('text-center', 'text-white');
+    resultadoDiv.appendChild(nombreCiudad)
+
+    resultadoDiv.appendChild(actual)
+    resultadoDiv.appendChild(tempMin)
+    resultadoDiv.appendChild(tempMax)
+
+
+    resultado.appendChild(resultadoDiv);
+
+}
+
+const limpirHTML = () => {
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+const kelvinACentigrados = grados => parseInt(grados - 273.15);
+
+function spinner() {
+    limpirHTML();
+
+    const divSpinner = document.createElement('div');
+    divSpinner.classList.add('sk-fading-circle');
+
+    divSpinner.innerHTML = `
+    
+    <div class="sk-circle1 sk-circle"></div>
+    <div class="sk-circle2 sk-circle"></div>
+    <div class="sk-circle3 sk-circle"></div>
+    <div class="sk-circle4 sk-circle"></div>
+    <div class="sk-circle5 sk-circle"></div>
+    <div class="sk-circle6 sk-circle"></div>
+    <div class="sk-circle7 sk-circle"></div>
+    <div class="sk-circle8 sk-circle"></div>
+    <div class="sk-circle9 sk-circle"></div>
+    <div class="sk-circle10 sk-circle"></div>
+    <div class="sk-circle11 sk-circle"></div>
+    <div class="sk-circle12 sk-circle"></div>
+  
+`;
+
+    resultado.appendChild(divSpinner);
+}
