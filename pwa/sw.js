80b00835a1bac49fee47e7004e898b24c5cec4dc
Tutin R <lokomilo1@gmail.com>
2020-11-15 00:00:28 -0300

    PWA

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,9 +1,29 @@
+const nombreCache = 'apv-v1';
+
+const archivos = [
+    './',
+    './error.html',
+    './index.html',
+    './css/bootstrap.css',
+    './css/styles.css',
+    './js/app.js',
+
+    
+];
+
+
 // cuando se instala el service worker
 
 self.addEventListener('install', e => {
     console.log('Instalando el serviceWorker');
 
-    console.log(e);
+    e.waitUntil(
+        caches.open(nombreCache)
+        .then(cache => {
+            console.log('cacheando');
+            cache.addAll(archivos)
+        })
+    )
 });
 
 
@@ -12,11 +32,28 @@ self.addEventListener('install', e => {
 self.addEventListener('activate', e => {
     console.log('Service Worker activado...');
 
-    console.log(e);
+    e.waitUntil(
+        caches.keys()
+        .then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== nombreCache) //filtra la version de cache actual
+                .map(key => caches.delete(key))  // borra als demas versiones de cache
+            )
+        })
+    )
 });
 
 //evento fech paa descargar archivos estaticos
 
-self.addEventListener('fetch', e => {
-    console.log('fetch...',e)
+self.addEventListener('fetch',  e => {
+    // console.log('fetch...',e)
+
+    e.respondWith(
+        caches.match(e.request)
+        .then(respuestaCache => {
+            console.log(e.request);
+            return respuestaCache || fetch(e.request);
+        })
+        .catch(()=> caches.match('./error.html'))
+    )
 })
